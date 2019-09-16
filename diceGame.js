2102d8373753ddcb79a15109604304cb0dfd2724
danny serra <danielserradev@gmail.com>
2019-09-16 14:52:08 -0500

    total scores

--- diceGame.js
+++ diceGame.js
@@ -1,28 +1,113 @@
 document.getElementById('myId').innerHTML="Dice Game";
 
-//DICE GAME
+//DiceCritCahllenge
+let totalScore = 0;
+
+function playDiceCRITChallenge(){
+  d4();
+  d6();
+  d8();
+  d10();
+  d20();
+  console.log("Your Score is" + " " + totalScore);
+}
+
 function diceRoll(side){
   result = Math.floor(Math.random()*side) + 1;
+  parseInt(result);
+  console.log(result);
   return result;
 }
 
-let diceResult = diceSelector()
-console.log("Your rolled a " + diceResult + " sided die and scored a " +  diceRoll(diceResult));
 
-function diceSelector(){
-  let isValid = false;
-  let size = "";
-  while (!isValid) {
-    size = prompt("Please enter the dice you would like to roll.");
-    if (size == "2") {
-      alert("Please enter a number greater than 2.");
+function d4(){
+  let crit = false;
+  let score = 0;
+  while (!crit) {
+    let result = diceRoll(4);
+    if (result !== 4) {
+      score = score + result;
     }
-    else if (size % 2 !== 0) {
-      alert("Please enter an even number.");
+    else{
+      crit = true;
+      console.log(" " + score);
+      totalScore = totalScore + score;
+      return score;
     }
-    else {
-      isValid = true;
-      return size;
+  }
+
+}
+
+
+function d6(){
+  let crit = false;
+  let score = 0;
+  while (!crit) {
+    let result = diceRoll(6);
+    if (result !== 6) {
+      score = score + result;
+    }
+    else{
+      crit = true;
+      console.log(" " + score);
+      totalScore = totalScore + score;
+      return score;
     }
   }
 }
+
+
+function d8(){
+  let crit = false;
+  let score = 0;
+  while (!crit) {
+    let result = diceRoll(8);
+    if (result !== 8) {
+      score = score + result;
+    }
+    else{
+      crit = true;
+      console.log(" " + score);
+      totalScore = totalScore + score;
+      return score;
+    }
+  }
+}
+
+
+function d10(){
+  let crit = false;
+  let score = 0;
+  while (!crit) {
+    let result = diceRoll(10);
+    if (result !== 10) {
+      score = score + result;
+    }
+    else{
+      crit = true;
+      console.log(" " + score);
+      totalScore = totalScore + score;
+      return score;
+    }
+  }
+}
+
+
+function d20(){
+  let crit = false;
+  let score = 0;
+  while (!crit) {
+    let result = diceRoll(20);
+    if (result !== 20) {
+      score = score + result;
+    }
+    else{
+      crit = true;
+      console.log(" " + score);
+      totalScore = totalScore + score;
+      return score;
+    }
+  }
+}
+
+playDiceCRITChallenge();
