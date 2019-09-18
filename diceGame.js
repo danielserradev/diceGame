321eeef6fa00aceefb7b2dac3da62c160b86601c
danny serra <danielserradev@gmail.com>
2019-09-18 16:03:13 -0500

    final copy

--- diceGame.js
+++ diceGame.js
@@ -1,5 +1,4 @@
 //DiceCritCahllenge
-
 function playDiceCRITChallenge(){
   let scores = [];
   let totalScores = [];
@@ -10,20 +9,14 @@ function playDiceCRITChallenge(){
   scores[3] = rollDice(10);
   scores[4] = rollDice(12);
   scores[5] = rollDice(20);
-
-  // poping out the total score from each dice
   for (let i = 0; i < scores.length; i++) {
     totalScores.push(scores[i][scores[i].length - 1]);
   }
-
-  // doubling the previous score if natural 1 was hit
   for (let i = 1; i < scores.length; i++) {
     if (scores[i][0] == 1) {
       finalScore += totalScores[i - 1];
     }
   }
-
-  // skipping the score of the next dice if natural crit was hit
   for (let i = 0; i < totalScores.length; i++) {
     if (totalScores[i] == 0 && totalScores[i + 1]) {
       i++;
@@ -34,9 +27,9 @@ function playDiceCRITChallenge(){
   }
   console.log("Your dice Totals are" + " " + totalScores);
   console.log("Your final Score is" + " " + finalScore);
-
   return finalScore;
 }
+
 function diceRoller(side){
   result = Math.floor(Math.random()*side) + 1;
   //console.log(result);
@@ -44,29 +37,28 @@ function diceRoller(side){
 }
 
 function rollDice(highestNumber){
-  let d4Results = [];
+  let diceResults = [];
   let crit = false;
   let score = 0;
   while (!crit) {
     let result = diceRoller(highestNumber);
-    d4Results.push(result);
+    diceResults.push(result);
     if (result !== highestNumber) {
       score = score + result;
     }
     else{
       crit = true;
-      d4Results.push(score);
-      console.log(d4Results);
-      console.log(" " + score);
-      return d4Results;
+      diceResults.push(score);
+      console.log(diceResults);
+      return diceResults;
     }
   }
 }
+
 function playerOne(){
   let playerName = prompt("What is your name player one?");
   document.getElementById("playerOneButton").innerHTML = playerName;
   let playerOneScore = playDiceCRITChallenge();
-  console.log(playerOneScore);
   let showScore = document.getElementById("playeroneScore");
   let p1score = document.getElementById('playerOneScore');
   p1score.innerHTML=playerName + " scored a " + " " + playerOneScore;
@@ -74,11 +66,11 @@ function playerOne(){
   let player = {Name: playerName, Score: playerOneScore};
   return player;
 }
+
 function playerTwo(){
   let playerName = prompt("What is your name player two?");
   document.getElementById("playerTwoButton").innerHTML = playerName;
   let playerTwoScore = playDiceCRITChallenge();
-  console.log(playerTwoScore);
   let showScore = document.getElementById("playerTwoScore");
   let p2score = document.getElementById('playerTwoScore');
   p2score.innerHTML=playerName + " scored a " + playerTwoScore;
@@ -86,11 +78,11 @@ function playerTwo(){
   let player = {Name: playerName, Score: playerTwoScore};
   return player;
 }
+
 function playerThree(){
   let playerName = prompt("What is your name player three?");
   document.getElementById("playerThreeButton").innerHTML = playerName;
   let playerThreeScore = playDiceCRITChallenge();
-  console.log(playerThreeScore);
   let showScore = document.getElementById("playerThreeScore");
   let p3score = document.getElementById('playerThreeScore');
   p3score.innerHTML=playerName + " scored a " + playerThreeScore;
@@ -98,11 +90,11 @@ function playerThree(){
   let player = {Name: playerName, Score: playerThreeScore};
   return player;
 }
+
 function playerFour(){
   let playerName = prompt("What is your name player four?");
   document.getElementById("playerFourButton").innerHTML = playerName;
   let playerFourScore = playDiceCRITChallenge();
-  console.log(playerFourScore);
   let showScore = document.getElementById("playerFourScore");
   let p4score = document.getElementById('playerFourScore');
   p4score.innerHTML=playerName + " scored a " + playerFourScore;
@@ -110,6 +102,7 @@ function playerFour(){
   let player = {Name: playerName, Score: playerFourScore};
   return player;
 }
+
 function getWinner(){
   let players = [];
   let player1 = playerOne();
@@ -125,7 +118,6 @@ function getWinner(){
   let lowest = players[0].Score;
   let wWCD = players[0].Name;
   for (let i = 0; i < players.length; i++) {
-    console.log(players[i].Score);
     if (players[i].Score < lowest) {
       lowest = players[i].Score;
       wWCD = players[i].Name;
@@ -135,5 +127,4 @@ let showScore = document.getElementById("getWinnersScore");
 let winner = document.getElementById('getWinner');
 winner.innerHTML=wWCD + " wins with the lowest score of" + " " + lowest;
 winner.style = "display: inline-block;";
-
 }
