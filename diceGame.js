808aa6b08a5573cdfab040994707409e7d6f3ce9
danny serra <danielserradev@gmail.com>
2019-09-18 15:42:27 -0500

    everything is done except 1 funtcion clean up

--- diceGame.js
+++ diceGame.js
@@ -1,5 +1,3 @@
-document.getElementById('myId').innerHTML="";
-
 //DiceCritCahllenge
 
 function playDiceCRITChallenge(){
@@ -64,37 +62,78 @@ function rollDice(highestNumber){
     }
   }
 }
-
 function playerOne(){
+  let playerName = prompt("What is your name player one?");
+  document.getElementById("playerOneButton").innerHTML = playerName;
   let playerOneScore = playDiceCRITChallenge();
   console.log(playerOneScore);
   let showScore = document.getElementById("playeroneScore");
-
   let p1score = document.getElementById('playerOneScore');
-  p1score.innerHTML=" Player 1 scored a" + " " + playerOneScore;
+  p1score.innerHTML=playerName + " scored a " + " " + playerOneScore;
   p1score.style = "display: inline-block;";
+  let player = {Name: playerName, Score: playerOneScore};
+  return player;
 }
 function playerTwo(){
+  let playerName = prompt("What is your name player two?");
+  document.getElementById("playerTwoButton").innerHTML = playerName;
   let playerTwoScore = playDiceCRITChallenge();
   console.log(playerTwoScore);
   let showScore = document.getElementById("playerTwoScore");
   let p2score = document.getElementById('playerTwoScore');
-  p2score.innerHTML=" Player 2 scored a" + " " + playerTwoScore;
+  p2score.innerHTML=playerName + " scored a " + playerTwoScore;
   p2score.style = "display: inline-block;";
+  let player = {Name: playerName, Score: playerTwoScore};
+  return player;
 }
 function playerThree(){
+  let playerName = prompt("What is your name player three?");
+  document.getElementById("playerThreeButton").innerHTML = playerName;
   let playerThreeScore = playDiceCRITChallenge();
   console.log(playerThreeScore);
   let showScore = document.getElementById("playerThreeScore");
   let p3score = document.getElementById('playerThreeScore');
-  p3score.innerHTML=" Player 3 scored a" + " " + playerThreeScore;
+  p3score.innerHTML=playerName + " scored a " + playerThreeScore;
   p3score.style = "display: inline-block;";
+  let player = {Name: playerName, Score: playerThreeScore};
+  return player;
 }
 function playerFour(){
+  let playerName = prompt("What is your name player four?");
+  document.getElementById("playerFourButton").innerHTML = playerName;
   let playerFourScore = playDiceCRITChallenge();
   console.log(playerFourScore);
   let showScore = document.getElementById("playerFourScore");
   let p4score = document.getElementById('playerFourScore');
-  p4score.innerHTML=" Player 4 scored a" + " " + playerFourScore;
+  p4score.innerHTML=playerName + " scored a " + playerFourScore;
   p4score.style = "display: inline-block;";
+  let player = {Name: playerName, Score: playerFourScore};
+  return player;
+}
+function getWinner(){
+  let players = [];
+  let player1 = playerOne();
+  let player2 = playerTwo();
+  let player3 = playerThree();
+  let player4 = playerFour();
+
+  players[0] = player1;
+  players[1] = player2;
+  players[2] = player3;
+  players[3] = player4;
+
+  let lowest = players[0].Score;
+  let wWCD = players[0].Name;
+  for (let i = 0; i < players.length; i++) {
+    console.log(players[i].Score);
+    if (players[i].Score < lowest) {
+      lowest = players[i].Score;
+      wWCD = players[i].Name;
+    }
+  }
+let showScore = document.getElementById("getWinnersScore");
+let winner = document.getElementById('getWinner');
+winner.innerHTML=wWCD + " wins with the lowest score of" + " " + lowest;
+winner.style = "display: inline-block;";
+
 }
